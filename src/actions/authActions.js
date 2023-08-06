1d824e5cc64b5bbd7b19e64e529c88105a159074
Mikosh2010 <mikosh.armanov@bk.ru>
2023-08-06 17:58:03 +0500

    frontend talks with backend

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,33 +1,60 @@
+// src/actions/authActions.js
+
+import axios from 'axios';
+
+const API_BASE_URL = 'http://localhost:8080/api/auth';
+
+// ... (ваш существующий код)
+
 export const login = (username, password) => {
-  return {
-    type: 'LOGIN',
-    payload: {
-      username,
-      password
-    }
+  return (dispatch) => {
+    axios
+      .post(`${API_BASE_URL}/signin`, {
+        username,
+        password,
+      })
+      .then((response) => {
+        if (response.status === 200 && response.data.loggedIn) {
+          dispatch({
+            type: 'LOGIN',
+            payload: {
+              username: response.data.username,
+            },
+          });
+        } else {
+          // Обработка ошибки, если авторизация не удалась
+        }
+      })
+      .catch((error) => {
+        // Обработка ошибки
+      });
   };
 };
 
 export const register = (username, email, password) => {
-  return {
-    type: 'REGISTER',
-    payload: {
-      username,
-      email,
-      password
-    }
+  return (dispatch) => {
+    axios
+      .post(`${API_BASE_URL}/signup`, {
+        username,
+        email,
+        password,
+      })
+      .then((response) => {
+        if (response.status === 200) {
+          dispatch({
+            type: 'REGISTER',
+            payload: {
+              username: response.data.username,
+            },
+          });
+        } else {
+          console.error('Ошибка при регистрации')
+        }
+      })
+      .catch((error) => {
+        // Обработка ошибки
+      });
   };
 };
 
-export const logout = () => {
-  return {
-    type: 'LOGOUT'
-  };
-};
-
-export const setRememberMe = (value) => {
-  return {
-    type: 'SET_REMEMBER_ME',
-    payload: value,
-  };
-};
+// ... (ваш существующий код)
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,7 +1,10 @@
-// src/redux/authReducer.js
 import Cookies from 'js-cookie';
 
-// reducers/authReducer.js
+// Определите новые типы действий (если они не определены в другом месте)
+export const LOGIN = 'LOGIN';
+export const REGISTER = 'REGISTER';
+export const LOGOUT = 'LOGOUT';
+export const SET_REMEMBER_ME = 'SET_REMEMBER_ME';
 
 const initialState = {
   isLoggedIn: Cookies.get('isLoggedIn') === 'true',
@@ -11,8 +14,10 @@ const initialState = {
 
 const authReducer = (state = initialState, action) => {
   switch (action.type) {
-    case 'LOGIN':
-      if (action.payload.username === 'mikosh' && action.payload.password === 'gRoove48') {
+    case LOGIN:
+      // Здесь можно добавить запрос к серверу для проверки логина и пароля
+      // Предположим, что сервер возвращает объект с полями loggedIn и username
+      if (action.payload.loggedIn) {
         Cookies.set('isLoggedIn', true, { expires: state.rememberMe ? 365 : 1 }); // Установите время действия cookies в 1 день или 1 год, в зависимости от rememberMe
         Cookies.set('username', action.payload.username); // Сохраняем username в куку
         return {
@@ -22,8 +27,9 @@ const authReducer = (state = initialState, action) => {
         };
       } else {
         return state;
-      };
-    case 'REGISTER':
+      }
+    case REGISTER:
+      // Здесь также можно добавить запрос к серверу для регистрации нового пользователя
       Cookies.set('isLoggedIn', true, { expires: state.rememberMe ? 365 : 1 }); // Установите время действия cookies в 1 день или 1 год, в зависимости от rememberMe
       Cookies.set('username', action.payload.username); // Сохраняем username в куку
       return {
@@ -31,11 +37,11 @@ const authReducer = (state = initialState, action) => {
         isLoggedIn: true,
         username: action.payload.username,
       };
-    case 'LOGOUT':
+    case LOGOUT:
       Cookies.remove('isLoggedIn'); // Удаляем куку isLoggedIn при выходе
       Cookies.remove('username'); // Удаляем куку username при выходе
-      return initialState;
-    case 'SET_REMEMBER_ME':
+      return { ...initialState, rememberMe: state.rememberMe }; // Возвращаем начальное состояние с сохранением rememberMe
+    case SET_REMEMBER_ME:
       Cookies.set('rememberMe', action.payload); // Сохраните значение rememberMe в cookies
       return {
         ...state,
