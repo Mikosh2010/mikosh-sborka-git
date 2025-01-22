36607aef678474d1650fbb9fdb349650605ea35c
Mikosh2010 <130504629+Mikosh2010@users.noreply.github.com>
2025-01-22 21:16:02 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-);
+setTimeout(() => window.interface('GameText').add("[1, 'YORENOV SCRIPT LOADED', 10000,0,-1,-1]"), 2000)
